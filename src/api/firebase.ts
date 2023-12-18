5e15c03b50ce941a4cee25a157862dfdf74a0bb0
Dario Puddinu <dario.puddinu@abinsula.com>
2023-12-18 16:30:23 +0100

    refactor: moved api to vehicle file, deleted vehicle.json

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { collection, deleteDoc, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc } from 'firebase/firestore/lite';
-import { VehicleType } from "./vehicle";
+import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
@@ -14,23 +13,3 @@ const firebaseConfig = {
 export const firebaseApp = initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-export const createVehicle = async (vehicle: VehicleType) => {
-  return await setDoc(doc(db, 'vehicles', vehicle.id), vehicle);
-};
-
-export const updateVehicle = async (vehicle: VehicleType) => {
-  const ref = doc(db, 'vehicles', vehicle.id);
-  return await updateDoc(ref, vehicle);
-};
-export const getVehicles = async () => {
-  return (await getDocs(collection(db, 'vehicles'))).docs.map((doc) => doc.data());
-};
-export const getVehicleById = async (id: string) => {
-  const ref = doc(db, 'vehicles', id);
-  return (await getDoc(ref)).data();
-};
-export const deleteVehicle = async (id: string) => {
-  const ref = doc(db, 'vehicles', id);
-  return await deleteDoc(ref);
-};
--- src/api/vehicle.ts
+++ src/api/vehicle.ts
@@ -1,7 +1,7 @@
 import { queryOptions } from '@tanstack/react-query';
 import axios from 'axios';
-import { collection, doc, getDocs, setDoc, updateDoc, deleteDoc } from 'firebase/firestore/lite';
 import { db } from './firebase';
+import { collection, deleteDoc, doc, getDoc, getDocs, setDoc, updateDoc } from 'firebase/firestore/lite';
 
 export type VehicleType = {
   id: string;
@@ -19,48 +19,23 @@ export type VehicleResponseType = {
 
 export class VehicleNotFoundError extends Error { }
 
-export const fetchVehicle = async (id: string) => {
-  const vehicle = await axios
-    .get<VehicleResponseType>('vehicles.json')
-    .then((r) => {
-      console.log(r.data);
-      const found = r.data.vehicles.find((x: VehicleType) => x.id === id);
-      console.log(found);
-      return found;
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.response.status === 404) {
-        throw new VehicleNotFoundError(`Vehicle with id "${id}" not found!`);
-      }
-      throw err;
-    });
-
-  return vehicle as VehicleType;
-};
-
-export const fetchVehicles = async () => {
-  const vehicles = collection(db, 'vehicles');
-  const vehicleSnapshot = await getDocs(vehicles);
-  const vehicleList = vehicleSnapshot.docs.map(doc => doc.data());
-  return vehicleList;
-};
-
 export const createVehicle = async (vehicle: VehicleType) => {
-  return await setDoc(doc(db, "vehicles", vehicle.id), vehicle);
+  return await setDoc(doc(db, 'vehicles', vehicle.id), vehicle);
 };
+
 export const updateVehicle = async (vehicle: VehicleType) => {
-  const ref = doc(db, "vehicles", vehicle.id);
+  const ref = doc(db, 'vehicles', vehicle.id);
   return await updateDoc(ref, vehicle);
 };
 export const getVehicles = async () => {
-  return await getDocs(collection(db, "vehicles"))
-}
+  return (await getDocs(collection(db, 'vehicles'))).docs.map((doc) => doc.data());
+};
 export const getVehicleById = async (id: string) => {
-  return await getDocs(collection(db, "vehicles", id))
-}
+  const ref = doc(db, 'vehicles', id);
+  return (await getDoc(ref)).data();
+};
 export const deleteVehicle = async (id: string) => {
-  const ref = doc(db, "vehicles", id);
+  const ref = doc(db, 'vehicles', id);
   return await deleteDoc(ref);
 };
 
